49b578a2f356130a448bb2714bf997903807fdff
Morgan MacFadyen <morganmacfadyen@Morgans-MacBook-Pro.local>
2017-10-15 01:54:12 +1300

    f

--- node-install.sh
+++ node-install.sh
@@ -8,7 +8,7 @@
 
 NVM_LINK="https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh"
 
-install_node_via_nvm(){
+upd_node(){
     ## CREATE A SYMBOLIC LINK TO NVM ##
 	sudo ln -s "${NVM_DIR}/nvm.sh" "/usr/local/bin" || sudo ln -s "${NVM_DIR}/nvm.sh" "/usr/bin" || echo "Failed to create symbolic link for nvm"
         
@@ -19,21 +19,16 @@ install_node_via_nvm(){
 	#CHECK NVM INSTALLED CORRECTLY by checking nvm --version
 	if [ -n ${NVM_VERS:-''} ] 
 	then
-		return 0 
-	else
-	   return 1
+	    if [ $(nvm ls ${1:-6} | grep N/A) ]; then
+        	nvm install ${1:-6} && nvm use ${1:-6}
+		else
+			nvm use ${1:-6} 
+		fi
+		return $?
 	fi
+	return 1s
 }
 
-install_node_version(){
-    source "${NVM_DIR}/nvm.sh"
-    if [ $(nvm ls ${1:-6} | grep N/A) ]; then
-        nvm install ${1:-6} && nvm use ${1:-6}
-    else
-        nvm use ${1:-6} 
-    fi
-    return $?
-}
 
  if ! [[ $NODE_VERSION  =~ ^[0-9]+.?[0-9]*.?[0-9]*$ ]]
  then
@@ -53,11 +48,12 @@ fi
 
 if ! [ -d "${NVM_DIR}" ] || ! [ -f "${NVM_DIR}/install.sh" ]; then
     which curl && curl -o- ${NVM_LINK} 2> /dev/null | bash || which wget && wget -qO- ${NVM_LINK} 2> /dev/null | bash &&
-    install_node_via_nvm && echo "succesfully installed nvm via curl..installing node" || (echo "error installing nvm via curl" && exit) && install_node_version $1 && 
-    echo "succesfully installed node verion ${1}" || echo "failed to instal node version ${1}"
+	echo "Succesfully installed nvm.. checking path" && upd_node $1 && 
+	echo "succesfully installed nvm via curl..installing node" ||
+    echo "Could not find nvm on path."
 else
-    echo "NVM is already installed on this machine"
-    install_node_version $1
+    echo "nvm is already installed. changing version."
+    upd_node $1
 fi
 
 exit
